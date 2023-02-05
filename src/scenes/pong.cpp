c88fe7e4e5dd99097b0a3af7f2308e77ee21d0d5
fowlmouth <eichholz.tim@gmail.com>
2023-02-04 20:31:40 -0600

    Fix ImGui code

--- src/scenes/pong.cpp
+++ src/scenes/pong.cpp
@@ -6,9 +6,9 @@ template<>
 void MM::ComponentEditorWidget< sg::components::Transform >(entt::registry& reg, entt::registry::entity_type entity)
 {
   auto& t = reg.get< sg::components::Transform >(entity);
-  ImGui::DragFlag("x##Transform", &t.x, 0.1f);
-  ImGui::DragFlag("y##Transform", &t.y, 0.1f);
-  ImGui::DragFlag("angle##Transform", &t.radians, 0.1f);
+  ImGui::DragFloat("x##Transform", &t.x, 0.1f);
+  ImGui::DragFloat("y##Transform", &t.y, 0.1f);
+  ImGui::DragFloat("angle##Transform", &t.radians, 0.1f);
 }
 
 struct PlayerScore
